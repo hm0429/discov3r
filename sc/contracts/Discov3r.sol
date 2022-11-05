0653208276ad7f3e96854359b160ba61f5f079b1
Hideyoshi Moriya <hide@piyo.jp>
2022-11-05 10:31:50 -0700

    close #20

--- sc/contracts/Discov3r.sol
+++ sc/contracts/Discov3r.sol
@@ -4,10 +4,15 @@ pragma solidity ^0.8.17;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./ITreasure.sol";
 import "./TreasureBox.sol";
+import "./IMidpoint.sol";
 
 contract Discov3r is Ownable, ITreasure {
     // TreasureBox contract
     TreasureBox public treasureBox;
+
+    // Midpoint
+    address constant startpointAddress = 0x47a4905D4C2Eabd58abBDFEcBaeB07F1A29b660c;
+    uint64 constant midpointID = 386;
 
     constructor() {
     }
@@ -56,6 +61,20 @@ contract Discov3r is Ownable, ITreasure {
         if (treasure.treasureType == TreasureType.Default) {
             payable(msg.sender).transfer(treasure.amount);
         }
+
         treasureBox.transferFrom(address(this), msg.sender, treasureId);
+    }
+
+    function unlockTreasureBox(
+        uint256 treasureId, 
+        string memory uuid,
+        string memory signature
+    ) 
+        external
+    {
+        require(treasureBox.exists(treasureId));
+        require(treasureBox.ownerOf(treasureId) == msg.sender);
+        bytes memory args = abi.encodePacked(signature, bytes1(0x00), uuid, bytes1(0x00));
+        IMidpoint(startpointAddress).callMidpoint(midpointID, args);
     }
 }
