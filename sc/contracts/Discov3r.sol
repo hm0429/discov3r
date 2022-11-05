54eb6c466c1fbda95d7ddeffb05fda4f769109d3
Hideyoshi Moriya <hide@piyo.jp>
2022-11-05 12:37:04 -0700

    only verified account can create treasure #14

--- sc/contracts/Discov3r.sol
+++ sc/contracts/Discov3r.sol
@@ -2,11 +2,17 @@
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "./ITreasure.sol";
 import "./TreasureBox.sol";
 import "./IMidpoint.sol";
 
 contract Discov3r is Ownable, ITreasure {
+    using ECDSA for bytes32;
+
+    address public signer = 0xa24CBE6d7a590E61e0271621f7A998308E469136;
+    mapping(address => bool) public verified;
+
     // TreasureBox contract
     TreasureBox public treasureBox;
 
@@ -14,7 +20,22 @@ contract Discov3r is Ownable, ITreasure {
     address constant startpointAddress = 0x47a4905D4C2Eabd58abBDFEcBaeB07F1A29b660c;
     uint64 constant midpointID = 386;
 
+    modifier onlyVerifiedAccount()
+    {
+        require(verified[msg.sender]);
+        _;
+    }
+
     constructor() {
+    }
+
+    // signature by discov3r signer is needed to create an account
+    function createAccount(bytes memory signature)
+        external
+    {
+        bytes32 hash = keccak256(abi.encodePacked(msg.sender)).toEthSignedMessageHash();
+        require(signer == hash.recover(signature));
+        verified[msg.sender] = true;
     }
 
     function setTreasureBoxContract(address _address) 
@@ -31,7 +52,8 @@ contract Discov3r is Ownable, ITreasure {
         bytes32 keyHash
     )
         public
-        payable 
+        payable
+        onlyVerifiedAccount
     {
          Treasure memory treasure = Treasure(
             msg.sender,             // address creator;
