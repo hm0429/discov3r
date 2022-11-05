19b97313a3f5f102d5e9b772c564a71c5d27dee6
Hideyoshi Moriya <hide@piyo.jp>
2022-11-05 14:58:26 -0700

    Update computeHash.js

--- scripts/computeHash.js
+++ scripts/computeHash.js
@@ -1,4 +1,4 @@
 const ethers = require('ethers');
-const MESSAGE = "piyo";
+const MESSAGE = "Code is Law";
 const hash = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(MESSAGE));
 console.log(hash)
